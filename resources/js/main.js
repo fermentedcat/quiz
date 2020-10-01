1c3c6aef17ce7742aa97ac7d6b00b9365d0fe145
Maja Thunberg <majaneko@gmail.com>
2020-10-01 20:44:51 +0200

    Now able to save answer true/false value

--- resources/js/main.js
+++ resources/js/main.js
@@ -5,139 +5,167 @@ document.addEventListener("DOMContentLoaded", function(e) {
     
 
 
-class Player {
-    constructor() {
-        this.name;
-        this.score; //// eller två, right/wrong
-    }
-
-
-}
-
-class Start {   //// steg 1 på sidan / håller i spelarens val
-    constructor() {
-        this.num_questions = 10;
-        this.category = "programming";
-        this.player = new Player()
-    }
-    newQuiz() {
-
-    }
-}
-
-class Quiz {
-    constructor(length, difficulty) {
-        this.length = length;
-        this.difficulty = difficulty;
-        this.questions;
-
-        
-        
-    }
-    startQuiz() { 
-        //// quiz api programming
-        fetch("https://quizapi.io/api/v1/questions?apiKey=C8BWSol6V6TUpmrsb7Zz17pdZoQuzcB9enTsztNA&limit=" + this.length + "&difficulty=" + this.difficulty)
-        .then(response => response.json())
-        .then(data => {
-            let questions = new Questions(data); //// send random questions to Questions to start
-            let answers = new Answers(data);
-
-            //TODO: nåt sätt att göra detta snyggare med sync/wait?
-            let answers_container = document.getElementById("answers_container");
-            let button = document.createElement("button");
-            button.innerHTML = "NEXT QUESTION";
-            answers_container.appendChild(button);
-            button.addEventListener("click", function(e) {
-
-            questions.changeQuestion();
-            answers.changeAnswers();
-            })
-        });
-    }
-    handleScore(score) {
-        alert(score);
-    }
-}
-
-class Questions {
-    constructor(fetched_obj) {
-        this.questions = fetched_obj;
-        for (let question of this.questions) {
-            console.log(question);
+   /*  class Player {
+        constructor() {
+            this.name;
+            this.score; //// eller två, right/wrong
+            this.score_arr = new Array(10);
         }
-        this.counter = 0; //// keeps track of which question to show
-
-        let q_container = document.getElementById("question_container");
-        this.currentQuestion = document.createElement("h1");
-        q_container.appendChild(this.currentQuestion);
-    }
-    
-
-    changeQuestion() {
-        console.log("change question!");
-        this.currentQuestion.innerHTML = this.questions[this.counter].question;
-        this.counter++;
-    }
-
-}
-
-class Answers {
-    constructor(fetched_obj) {
-        this.answers = fetched_obj;
-        this.counter = 0;
-        this.path_array = ["answer_a", "answer_b", "answer_c", "answer_d", "answer_e", "answer_f"];
-        this.is_correct_path_arr = this.path_array.map(function(path) {
-            return path + "_correct";
-        })
-        this.options = [];
-
-        this.answers_container = document.getElementById("list");
-        for (let i = 0; i < 6; i++) {
-            let option = document.createElement("li");
-            this.options.push(option);
+        handleScore(is_correct, index) {
+            this.score_arr[index] = is_correct;
+            console.log(this.score_arr);
         }
-        
-    }
-    changeAnswers() {
-        console.log("change answers!");
-        console.log(this.answers);
-        for (let i = 0; i < 6; i++) {
-            let answer = this.answers[this.counter].answers[this.path_array[i]];
-            console.log(typeof answer);
-            if (answer != null) {
-                answer = answer.replace(/\</g,"&lt;");
 
-                console.log("Answer:" + answer);
-                this.options[i].innerHTML = answer;
-                this.answers_container.appendChild(this.options[i]);
-                this.options[i].style.display = "";
-                this.options[i].addEventListener("click", function(e) {
-                    // quiz.handleScore(this.answers[this.counter].correct_answers[i]);
-                    console.log(this.answers[this.counter].correct_answers[this.is_correct_path_arr[i]]);
+    } */
+
+    /* class Start {   //// steg 1 på sidan / håller i spelarens val
+        constructor() {
+            this.num_questions = 10;
+            this.category = "programming";
+            this.player = new Player()
+        }
+        newQuiz() {
+
+        }
+    } */
+
+    class Quiz {
+        constructor(length, difficulty, category) {
+            this.length = length;
+            this.difficulty = difficulty;
+            this.category = category;
+            this.questions;
+
+            
+            
+        }
+        startQuiz() { 
+            //// quiz api programming
+            fetch("https://quizapi.io/api/v1/questions?apiKey=C8BWSol6V6TUpmrsb7Zz17pdZoQuzcB9enTsztNA&limit=" + this.length + "&difficulty=" + this.difficulty + "&tags=" + this.category)
+            .then(response => response.json())
+            .then(data => {
+                let questions = new Questions(data); //// send random questions to Questions to start
+                let answers = new Answers(data);
+
+                //TODO: nåt sätt att göra detta snyggare med sync/wait?
+                let status_bar = document.getElementById("status_bar");
+                
+                let back_button = document.createElement("i");
+                back_button.className = "fas fa-chevron-circle-left fa-2x";
+                status_bar.appendChild(back_button);
+                back_button.addEventListener("click", function(e) {
+                    if (questions.counter > 1 ) {
+                        questions.changeQuestion("back");
+                        answers.changeAnswers("back");
+                    }
                 })
-            } else {
-                this.options[i].style.display = "none";
-            }
+                let show_number = document.createElement("p");
+                show_number.id = "show_number"
+                show_number.innerHTML = questions.counter + " / " + this.length;
+                status_bar.appendChild(show_number);
+
+                let button = document.createElement("i");
+                button.className = "fas fa-chevron-circle-right fa-2x";
+                status_bar.appendChild(button);
+                button.addEventListener("click", event => {
+                    if (questions.counter < this.length) {
+                        questions.changeQuestion("fwd");
+                        answers.changeAnswers("fwd");
+                    }
+
+                })
+            });
         }
-        this.counter++;
     }
-}
 
-let quiz = new Quiz(10, "Easy");
-    quiz.startQuiz();
+    class Questions {
+        constructor(fetched_obj) {
+            this.questions = fetched_obj;
+            for (let question of this.questions) {
+                console.log(question);
+            }
+            this.counter = 0; //// keeps track of which question to show
 
-})
+            let q_container = document.getElementById("question_container");
+            this.currentQuestion = document.createElement("h1");
+            q_container.appendChild(this.currentQuestion);
+        }
+        
 
-/* class Question {
-    constructor() {
-        this.question = question;
-        this.correct_answer = correct_answer;
-        this.alt_1 = alt_1;
-        this.alt_2 = alt_2;
-        this.alt_3 = alt_3;
+        changeQuestion(step) {
+            if (step == "back") {
+                this.counter -= 2;
+            }
+            let question = this.questions[this.counter].question;
+            question = question.replace(/\</g,"&lt;");
+            this.currentQuestion.innerHTML = question;
+            this.counter++;
+            document.getElementById("show_number").innerHTML = this.counter + " / " + this.questions.length;
+        }
+
+    }
+
+    class Answers {
+        constructor(fetched_obj) {
+            this.answers = fetched_obj;
+            this.counter = 0;
+            this.option_paths = ["answer_a", "answer_b", "answer_c", "answer_d", "answer_e", "answer_f"];
+            this.is_correct_paths = this.option_paths.map(function(path) {
+                return path + "_correct";
+            })
+            this.player_choices = new Array(fetched_obj.length).fill(false); // rätt/fel svar sparas
+            
+            this.answers_container = document.getElementById("list");
+                
+            
+        }
+        changeAnswers(step) { 
+            while (this.answers_container.firstChild) { // ta bort alla svarsalt från förra frågan
+                this.answers_container.removeChild(this.answers_container.lastChild);
+            }
+            if (step == "back") {
+                this.counter -= 2;
+            }
+            for (let i = 0; i < 6; i++) {
+                let answer = this.answers[this.counter].answers[this.option_paths[i]];
+                if (answer != null) { // answers with value null should not be displayed
+                    let option = document.createElement("li");
+                    
+                    let is_correct = this.answers[this.counter].correct_answers[this.is_correct_paths[i]]; // this option is    true = correct || false = incorrect
+                    
+                    let regex_answer = answer.replace(/\</g,"&lt;"); 
+                    option.innerHTML = regex_answer;
+                    this.answers_container.appendChild(option);
+                    option.addEventListener("click", event => {
+                        this.player_choices[this.counter - 1] = is_correct; 
+                        // counter redan +1. sparar val till array i constructor
+                    })
+                    
+                } 
+            }
+            this.counter++;
+        }
+        checkAnswer() {
+            
+        }
         
     }
-    
-} */
-/* let questions = new Questions();
-questions.changeQuestion("hello"); */
+    let quiz = new Quiz(10, "Easy", "JavaScript");
+    quiz.startQuiz();
+    /* let player = new Player(); */
+
+
+    /* class Question {
+        constructor() {
+            this.question = question;
+            this.correct_answer = correct_answer;
+            this.alt_1 = alt_1;
+            this.alt_2 = alt_2;
+            this.alt_3 = alt_3;
+            
+        }
+        
+    } */
+    /* let questions = new Questions();
+    questions.changeQuestion("hello"); */
+});
