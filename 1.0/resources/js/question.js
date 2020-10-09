d58977c0c62f81687f7f8dd2de7cee81b62effb3
Maja Thunberg <majaneko@gmail.com>
2020-10-09 13:51:35 +0200

    Deleted version 1.0

--- 1.0/resources/js/question.js
+++
@@ -1,9 +0,0 @@
-class Question {
-    constructor(question) {
-        this.question = question.question;
-        this.answers = question.answers;
-        this.correct_answers = question.correct_answers; 
-        this.category = question.tags[0];
-        this.difficulty = question.difficulty;
-    }
-}
